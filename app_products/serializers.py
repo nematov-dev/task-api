bc51e9b8f8e458bb4623e670b8e95aefcd1d3280
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-02-08 17:14:47 +0500

    Change seralizers.py

--- app_products/serializers.py
+++ app_products/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from app_products.models import PostsModel
+from app_products.models import ProductsModel
 
 
 class ProductSerialize(serializers.ModelSerializer):
     class Meta:
-        model = PostsModel
+        model = ProductsModel
         fields = '__all__'
