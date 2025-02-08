3ef5b05123ac4a179b6a17ebea6e4ae066988b6f
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-02-08 17:13:14 +0500

    Change url

--- Conf/urls.py
+++ Conf/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/products/',include('app_products',namespace='products')),
+    path('api/v1/products/',include('app_products.urls',namespace='products')),
 ]
